3a2a2bb111b23619af0c0fcb6544cbba4d11e8d6
Arm Bun <bellotaophyc@outlook.com>
2022-08-08 19:33:32 +0000

    starts a new

--- main.tf
+++ main.tf
@@ -1,13 +1,15 @@
-provider "aws" {
-    region = "us-east-2"
+    provider "aws" {
+        version = "~> 2.0"
+        region = "us-east-2"
 }
 
-resource "aws_instance" "warmup" {
-    ami     = "ami-0c55b159cbfafe1f0"
-    instance_type = "t2.micro"
+    resource "aws_instance" "warmup" {
+        ami     = "ami-0c55b159cbfafe1f0"
+        instance_type = "t2.micro"
 
-    tags = {
-    Name = "warmup_instance"
-}
+        tags = {
+        Name = "warmup_instance"
+    }
+
 }
 
