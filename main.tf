ac0b1b8bd982b1eb6c95eca87ba8eb20578fb6ed
Arm Bun <bellotaophyc@outlook.com>
2022-08-09 11:51:19 +0000

    a single web server

--- main.tf
+++ main.tf
@@ -5,6 +5,13 @@
     resource "aws_instance" "warmup" {
         ami     = "ami-0c55b159cbfafe1f0"
         instance_type = "t2.micro"
+        vpc_security_group_ids = [aws_security_group.instance.id]
+
+        user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p 8080 &
+                    EOF
 
         tags = {
         Name = "warmup_instance"
@@ -12,3 +19,14 @@
 
 }
 
+resource "aws_security_group" "instance" {
+    name = "terraform-warmup-instance"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+  
+}
