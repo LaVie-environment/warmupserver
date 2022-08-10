16984e6578d3c3746c443ff87904b42bb424b18b
Arm Bun <bellotaophyc@outlook.com>
2022-08-10 11:01:15 +0000

    Deploying a cluster of web servers

--- main.tf
+++ main.tf
@@ -2,6 +2,7 @@
         region = "us-east-2"
 }
 
+/*
     resource "aws_instance" "warmup" {
         ami     = "ami-0c55b159cbfafe1f0"
         instance_type = "t2.micro"
@@ -41,3 +42,54 @@
       value = aws_instance.warmup.public_ip
       description = "The public IP address of the web server"
     }
+
+*/
+
+# Auto Scaling Group 
+
+    resource "aws_launch_configuration" "warmup" {
+      image_id = "ami-0c55b159cbfafe1f0"
+      instance_type = "t2.micro"
+      security_groups = [aws_security_group.instance.id]
+
+      user_data = <<-EOF
+                    #!/bin/bash
+                    echo "Hello, World" > index.html
+                    nohup busybox httpd -f -p ${var.server_port} & 
+                    EOF
+
+    # Required when using a launch configuration with an auto scaling group.
+    # https://www.terraform.io/docs/providers/aws/r/launch_configuration.html
+
+    lifecycle {
+        create_before_destroy = true
+    }
+}
+
+     variable "server_port" {
+        description = "The port the server will use for HTTP requests"
+        type = number
+        default = 8080
+}
+
+    resource "aws_autoscaling_group" "warmup-asg" {
+      launch_configuration = aws_launch_configuration.warmup-asg.name
+      vpc_zone_identifier = data.aws_subnet_ids.default.ids
+
+      min_size = 2
+      max_size = 10
+
+      tag {
+        key = "Name"
+        value = "terraform-asg-warmup-asg"
+        propagate_at_launch = true
+      }
+}
+
+    data "aws_vpc" "default" {
+      default = true
+}
+
+    data "aws_subnet_ids" "default" {
+        vpc_id = data.aws_vpc.default.id 
+}
